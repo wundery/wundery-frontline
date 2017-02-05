74e637e549cb1eaddf6dbb7ad5cc4f5bf1b8e8d6
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-02-05 17:21:48 +0100

    Update translations

--- src/translations/de.js
+++ src/translations/de.js
@@ -1,15 +1,30 @@
 export default {
   features: {
-    cartbox: {
+    cart: {
       containers: {
         cartbox: {
           cart: 'Warenkorb',
         },
       },
     },
-    searchbox: {
+    cookieBanner: {
       containers: {
-        searchbox: {
+        cookieBanner: {
+          info: 'Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.',
+          acknowledge: 'Verstanden',
+          more: 'Weitere Informationen',
+        },
+      },
+    },
+    search: {
+      components: {
+        searchResults: {
+          placeholder: 'Suche',
+          noResultsHeadline: 'Es wurden keine Produkte gefunden',
+          oneResultHeadline: 'Es wurde ein Produkt gefunden',
+          resultsHeadline: 'Es wurden ${resultsCount} Produkte gefunden',
+        },
+        searchInput: {
           placeholder: 'Suche',
         },
       },
