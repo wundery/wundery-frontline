1c0e353ec93e1a91ffc5af6cc5e90d75ba4fb40f
Nick NUS <nck@nustechnology.com>
2019-07-30 15:52:23 +0700

    Adding support for localhost testing

--- src/features/Search/components/SearchResults.js
+++ src/features/Search/components/SearchResults.js
@@ -18,7 +18,11 @@ function SearchResults({ results, resultsRef, term, t, showDescription }) {
   }
 
   function searchURL(){
-    return window.location.origin + "/search&q=" + encodeURIComponent(term);
+    var queryPath = "search&q=" + encodeURIComponent(term);
+    if (window.location.hostname == "localhost")
+      return window.location.href.match(/\S*fragment=/g)[0] + queryPath
+    else
+      return window.location.origin + '/' + queryPath
   }
 
   return results && results.length > 0 && (
