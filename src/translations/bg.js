71d45a71129dff15beeb4f61467d45d7d29c0531
Jack@NUS <jack@nustechnology.com>
2020-05-12 10:57:27 +0700

    v1.0.17: Add bungarian translation

---
+++ src/translations/bg.js
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+/* eslint-disable no-template-curly-in-string */
+
+export default {
+  features: {
+    cart: {
+      containers: {
+        cartbox: {
+          cart: 'Количка',
+        },
+      },
+    },
+    cookieBanner: {
+      containers: {
+        cookieBanner: {
+          info: 'Този уебсайт използва бисквитки.',
+          acknowledge: 'Разбирам и приемам',
+          more: 'Повече информация',
+        },
+      },
+    },
+    search: {
+      components: {
+        searchResults: {
+          noResultsHeadline: 'Няма намерени продукти',
+          oneResultHeadline: 'Един продукт е открит',
+          resultsHeadline: '${resultsCount} продукти са открити',
+          seeAll: 'Виж всички (${count}) резултати'
+        },
+        searchInput: {
+          placeholder: 'Търси',
+        },
+      },
+    },
+  },
+};
