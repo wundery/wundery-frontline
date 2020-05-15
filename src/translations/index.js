742cdf7867a00ec9ce32b8838ed5b09b3ed3f647
Ken NUS <ken@nustechnology.com>
2020-05-15 10:16:29 +0700

    v1.0.19: add italy translation

--- src/translations/index.js
+++ src/translations/index.js
@@ -2,3 +2,4 @@ export de from './de';
 export en from './en';
 export bg from './bg';
 export es from './es';
+export it from './it';
---
+++ src/translations/it.js
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+/* eslint-disable no-template-curly-in-string */
+
+export default {
+  features: {
+    cart: {
+      containers: {
+        cartbox: {
+          cart: 'Carrello',
+        },
+      },
+    },
+    cookieBanner: {
+      containers: {
+        cookieBanner: {
+          info: 'Questo sito utilizza cookies.',
+          acknowledge: 'Ok, ho capito',
+          more: 'Maggiori informazioni',
+        },
+      },
+    },
+    search: {
+      components: {
+        searchResults: {
+          noResultsHeadline: 'Nessun prodotto trovato',
+          oneResultHeadline: 'Un prodotto trovato',
+          resultsHeadline: 'Sono stati trovati ${resultsCount} prodotti',
+          seeAll: 'Visualizza tutti i (${count}) risultati'
+        },
+        searchInput: {
+          placeholder: 'Ricerca',
+        },
+      },
+    },
+  },
+};
