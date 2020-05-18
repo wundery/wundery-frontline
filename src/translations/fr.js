5f806e1466d627adfb148594be6bf1c5c0e90899
Jack@NUS <jack@nustechnology.com>
2020-05-18 11:09:21 +0700

    v1.0.21: Add French translation

---
+++ src/translations/fr.js
@@ -0,0 +1,37 @@
+﻿/* eslint-disable max-len */
+/* eslint-disable no-template-curly-in-string */
+
+
+export default {
+  features: {
+    cart: {
+      containers: {
+        cartbox: {
+          cart: 'Carte',
+        },
+      },
+    },
+    cookieBanner: {
+      containers: {
+        cookieBanner: {
+          info: 'Ce site utilise des cookies.',
+          acknowledge: 'Ok, j’ai compris',
+          more: 'Plus d’informations',
+        },
+      },
+    },
+    search: {
+      components: {
+        searchResults: {
+          noResultsHeadline: 'Aucun produit trouvé',
+          oneResultHeadline: 'Un produit trouvé',
+          resultsHeadline: '${resultsCount} produits ont été trouvés',
+          seeAll: 'Voir tous les résultats (${count})'
+        },
+        searchInput: {
+          placeholder: 'Recherche',
+        },
+      },
+    },
+  },
+};
--- src/translations/index.js
+++ src/translations/index.js
@@ -4,3 +4,4 @@ export bg from './bg';
 export es from './es';
 export it from './it';
 export ar from './ar';
+export fr from './fr';
