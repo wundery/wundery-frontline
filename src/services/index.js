3f40cfe2fae8bdae3fba53c553f766aac5ba0ae4
Ken Branchbob <ken@branchbob.com>
2024-10-25 17:47:40 +0700

    Test integrating elasticsearch

--- src/services/index.js
+++ src/services/index.js
@@ -2,3 +2,5 @@ export Cart from './Cart';
 export CookieBanner from './CookieBanner';
 export Frontline from './Frontline';
 export Search from './Search';
+export ElasticSearch from './ElasticSearch';
+
