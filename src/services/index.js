f8a56a76af0a1a87bd72e27b81c538fe8eca72c7
Ken Branchbob <ken@branchbob.com>
2024-10-30 23:52:32 +0700

    Cleanup redundant code

--- src/services/index.js
+++ src/services/index.js
@@ -2,5 +2,4 @@ export Cart from './Cart';
 export CookieBanner from './CookieBanner';
 export Frontline from './Frontline';
 export Search from './Search';
-export ElasticSearch from './ElasticSearch';
 
