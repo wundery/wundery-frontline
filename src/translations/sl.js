32b918ab56e51c9f9a115d5d6d0d692be76caca3
Ken Diep <ken@branchbob.com>
2024-11-20 09:49:59 +0700

    Add Slovenian (sl) language to storefront

---
+++ src/translations/sl.js
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+/* eslint-disable no-template-curly-in-string */
+
+export default {
+  features: {
+    cart: {
+      containers: {
+        cartbox: {
+          cart: "Košarica",
+        },
+      },
+    },
+    cookieBanner: {
+      containers: {
+        cookieBanner: {
+          info: "Ta spletna stran uporablja piškotke.",
+          acknowledge: "V redu, razumem",
+          more: "Več informacij",
+        },
+      },
+    },
+    search: {
+      components: {
+        searchResults: {
+          noResultsHeadline: "Ni najdenih izdelkov",
+          oneResultHeadline: "Najden je en izdelek",
+          resultsHeadline: "${resultsCount} izdelkov je bilo najdenih",
+          seeAll: "Poglejte vse rezultate (${count})",
+        },
+        searchInput: {
+          placeholder: "Išči",
+        },
+      },
+    },
+  },
+};
